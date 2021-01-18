9f951e258d6e06211ac23f7c6902174e0c3fe10b
Carina Blair <cdblairnz@gmail.com>
2021-01-18 17:12:57 +0100

    Style the Drama list and its list elements

--- src/components/DramaRow.tsx
+++ src/components/DramaRow.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import Thumbnail from "./Thumbnail";
+import React from 'react';
+import Thumbnail from './Thumbnail';
+import './DramaRow.css';
 
-export interface DramaRowProps{
+export interface DramaRowProps {
     dramas: Drama[];
 }
 
-export interface Drama{
+export interface Drama {
     imageUrl: string;
-    title: string;  
+    title: string;
 }
 
 export const DramaRow: React.FC<DramaRowProps> = (props) => {
-    
-    return ( 
-    <>
-    {props.dramas.map(drama => <Thumbnail imageUrl={drama.imageUrl} title={drama.title}/>)}
-    </>
+
+    return (
+        <ul style={{ display: 'flex', listStyle: 'none' }}>
+            {props.dramas.map(drama => <li style={{ margin: 10 }}><Thumbnail imageUrl={drama.imageUrl} title={drama.title} /></li>)}
+        </ul>
     );
 };
 
