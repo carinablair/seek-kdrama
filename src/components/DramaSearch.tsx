eb2d56a05d872c205a3efac8e1f3d688ed367eb7
Carina Blair <cdblairnz@gmail.com>
2021-01-29 15:59:52 +0100

    Prevent the form from refreshing onSubmit

--- src/components/DramaSearch.tsx
+++ src/components/DramaSearch.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react';
 
 export const DramaSearch: React.FC = () => {
+
+    const searchDramas = async (event: React.FormEvent) => {
+        event.preventDefault();
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchDramas}>
             <label className="label" htmlFor="query"> Kdrama Name</label>
             <input className="input" type="text" name="query" placeholder="i.e. Crash Landing on You"></input>
             <button className="button" type="submit" >Search</button>
